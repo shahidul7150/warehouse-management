eb690949fd979a70c4bb1a42e662fbcb16fb9745
Shahidul Islam <shahidulislam7150@gmail.com>
2022-04-29 16:46:55 +0600

    add nav and set route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Blogs from './components/Blogs/Blogs';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+   <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='home' element={<Home></Home>}></Route>
+        <Route path='about' element={<About></About>}></Route>
+        <Route path='blogs' element={<Blogs></Blogs>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-white">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <Link className="navbar-brand " to="/">
+              F2C STORE
+            </Link>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-5">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="home">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="about">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link "
+                  to="blogs"
+                  tabindex="-1"
+                  aria-disabled="true"
+                >
+                  Blogs
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
