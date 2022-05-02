8fc3ccaf72ea1999e2b4653bd69fbf63c9ba373e
Shahidul Islam <shahidulislam7150@gmail.com>
2022-05-02 10:53:36 +0600

    client item add complated

--- src/components/AddService/AddService.js
+++ src/components/AddService/AddService.js
@@ -2,15 +2,32 @@ import React from "react";
 import { useForm } from "react-hook-form";
 const AddService = () => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+    const onSubmit = (data) => {
+        
+        console.log(data)
+        const url = `http://localhost:5000/service`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(itemData => {
+            console.log(itemData);
+        })
+    };
+    
   return (
-    <div>
-      Add service
+    <div className="w-25 mx-auto mt-5">
+      <h4>Add service</h4>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("firstName", { required: true, maxLength: 20 })} />
-        <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-        <input type="number" {...register("age", { min: 18, max: 99 })} />
-        <input type="submit" />
+        <input className="d-block w-100 mb-3" {...register("name", { required: true, maxLength: 20 })} placeholder="Name"/>
+        <textarea className="d-block w-100 mb-3" {...register("description")} placeholder="Description" />
+        <input className="d-block w-100 mb-3" type="number" {...register("price")} placeholder="Price"/>
+        <input className="d-block w-100 mb-3" type="text" {...register("img")} placeholder="Photo URL"/>
+        <input className="btn btn-success" type="submit" value="Add item" />
       </form>
     </div>
   );
