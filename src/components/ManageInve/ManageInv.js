a88fbfb9226e3d4c41683cb68390fd8513da4ae8
Shahidul Islam <shahidulislam7150@gmail.com>
2022-05-02 12:07:48 +0600

    load items on manage inventory page from server

--- src/components/ManageInve/ManageInv.js
+++ src/components/ManageInve/ManageInv.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import useProducts from "../hooks/useProduct";
 
 const ManageInv = () => {
-    return (
-        <div>
-            <h2>Manage inventory</h2>
+  const [products] = useProducts();
+  return (
+    <div className="w-50 mx-auto">
+      <h2 className="mb-4 mt-5">Manage inventory</h2>
+      {products.map((product) => (
+          <div className="d-flex justify-content-between align-items-center border p-2 mb-2">
+              <div className="d-flex justify-content-start align-items-center" key={product._id}>
+          <img width="50px" height="50px" className="me-4" src={product.img} alt="" />
+              <div>
+              <h4>{product.name}</h4>
+          <h4>Price: ${product.price}</h4>
+          <p>{product.description}</p>
+          </div>
+              </div>
+              
+              <div className="w-25">
+                  <button  className="mb-2 btn btn-danger  w-50">X</button>
+                  <br />
+                  <button className=" btn btn-primary w-50">Edit</button>
+              </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default ManageInv;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
+import useProducts from "../hooks/useProduct";
 import Product from "../Product/Product";
 import './Products.css'
 const Products = () => {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:5000/service")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
-  }, []);
+  const [products]=useProducts()
+  // const [products, setProducts] = useState([]);
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/service")
+  //     .then((res) => res.json())
+  //     .then((data) => setProducts(data));
+  // }, []);
   return (
-    <div className='products-container'>
+    <div className='products-container px-3'>
           {
               products.map(product=><Product key={product._id} product={product}></Product>)
       }
