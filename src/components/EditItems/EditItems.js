6bca4efbb2af9b0084f13a52ca9f55a1d6529f79
Shahidul Islam <shahidulislam7150@gmail.com>
2022-05-04 23:20:24 +0600

    product update add

--- src/components/EditItems/EditItems.js
+++ src/components/EditItems/EditItems.js
@@ -1,24 +1,82 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useParams } from "react-router-dom";
 
-import useProducts from '../hooks/useProduct';
-
-
+import useProducts from "../hooks/useProduct";
 
 const EditItems = () => {
-    const {updateId}=useParams()
-    const [products,setProducts] = useProducts();
-    useEffect(() => {
-        const url = `http://localhost:5000/service/${updateId}`
-        fetch(url)
-            .then(res => res.json())
-            .then(data => setProducts(data));
-    },[])
-    return (
-        <div>
-            <h3>Edit items {products.name}</h3>
-        </div>
-    );
+  const { updateId } = useParams();
+  console.log(updateId);
+
+    const [products, setProducts] = useProducts();
+    console.log(products.name);
+  // const [products,setProducts] = useProducts();
+  useEffect(() => {
+    const url = `http://localhost:5000/service/${updateId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data, event) => {
+    console.log(data);
+    const url = `http://localhost:5000/service/${updateId}`;
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((itemData) => {
+        console.log(itemData);
+      });
+    event.target.reset();
+  };
+  return (
+      <div>
+          
+      <h3 className="text-center mb-5">Edit items {products.name}</h3>
+
+      <form className="w-25 mx-auto" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className="d-block w-100 mb-3"
+          {...register("name", { required: true, maxLength: 20 })}
+                  placeholder="Name"
+                  required
+        />
+        <textarea
+          className="d-block w-100 mb-3"
+          {...register("description")}
+                  placeholder="Description"
+                  required
+        />
+        <input
+          className="d-block w-100 mb-3"
+          type="number"
+          {...register("quantity")}
+                  placeholder="quantity"
+                  required
+        />
+        <input
+          className="d-block w-100 mb-3"
+          type="number"
+          {...register("price")}
+                  placeholder="Price"
+                  required
+        />
+        <input
+          className="d-block w-100 mb-3"
+          type="text"
+          {...register("img")}
+                  placeholder="Photo URL"
+                  required
+        />
+        <input className="btn btn-success" type="submit" value="Update item" />
+      </form>
+    </div>
+  );
 };
 
 export default EditItems;
--- src/components/ManageInve/ManageInv.js
+++ src/components/ManageInve/ManageInv.js
@@ -3,48 +3,66 @@ import { useNavigate } from "react-router-dom";
 import useProducts from "../hooks/useProduct";
 
 const ManageInv = () => {
-    const [products,setProducts] = useProducts();
-    const navigate=useNavigate()
-    const handleDelete = id => {
-       
-        const proceed = window.confirm("Are you sure want to delete?");
-        if(proceed){
-            const url = `http://localhost:5000/service/${id}`;
-            fetch(url, {
-                method:"DELETE"
-            })
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data);
-                    const remaining = products.filter(product => product._id !== id);
-                    setProducts(remaining)
-            })
-        }
+  const [products, setProducts] = useProducts();
+  const navigate = useNavigate();
+  const handleDelete = (id) => {
+    const proceed = window.confirm("Are you sure want to delete?");
+    if (proceed) {
+      const url = `http://localhost:5000/service/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          const remaining = products.filter((product) => product._id !== id);
+          setProducts(remaining);
+        });
     }
+  };
 
-    const navigateEditDetail = id => {
-        navigate(`/update/${id}`)
-    }
+  const navigateEditDetail = (id) => {
+    navigate(`/update/${id}`);
+  };
   return (
     <div className="w-50 mx-auto">
       <h2 className="mb-4 mt-5">Manage inventory</h2>
       {products.map((product) => (
-          <div className="d-flex justify-content-between align-items-center border p-2 mb-2">
-              <div className="d-flex justify-content-start align-items-center" key={product._id}>
-          <img width="50px" height="50px" className="me-4" src={product.img} alt="" />
-              <div>
-                      <h4>{product.name}</h4>
-          <h4>Price: ${product.price}</h4>
-                      <small>Available: {product.quantity}</small>
-          <p>{product.description}</p>
+        <div  key={product._id} className="d-flex justify-content-between align-items-center border p-2 mb-2">
+          <div
+            className="d-flex justify-content-start align-items-center"
+           
+          >
+            <img
+              width="50px"
+              height="50px"
+              className="me-4"
+              src={product.img}
+              alt=""
+            />
+            <div>
+              <h4>{product.name}</h4>
+              <h4>Price: ${product.price}</h4>
+              <small>Available: {product.quantity}</small>
+              <p>{product.description}</p>
+            </div>
           </div>
-              </div>
-              
-              <div className="w-25">
-                  <button onClick={()=>handleDelete(product._id)} className="mb-2 btn btn-danger  w-50">X</button>
-                  <br />
-                  <button onClick={()=>navigateEditDetail(product._id)} className=" btn btn-primary w-50">Edit</button>
-              </div>
+
+          <div className="w-25">
+            <button
+              onClick={() => handleDelete(product._id)}
+              className="mb-2 btn btn-danger  w-50"
+            >
+              X
+            </button>
+            <br />
+            <button
+              onClick={() => navigateEditDetail(product._id)}
+              className=" btn btn-primary w-50"
+            >
+              Edit
+            </button>
+          </div>
         </div>
       ))}
     </div>
