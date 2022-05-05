f9ca2d40c1a5eadeb154fc4146fc6adf0bf8a0b7
Shahidul Islam <shahidulislam7150@gmail.com>
2022-05-05 20:43:05 +0600

    add about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            about
+        <div className='w-50 mx-auto'>
+            <h3>About C2FSTORE </h3>
+            <p>C2F is the grocery product suppler.The organization grew up with a brand of organic vegetables.We always want to satisfy our daily customers .We never supply formalin including product .Our product is 100% safe for human body. </p>
+            <p>We start our business 2012.Now we are one of the famous organic food supplier in the Bangladesh.</p>
         </div>
     );
 };
