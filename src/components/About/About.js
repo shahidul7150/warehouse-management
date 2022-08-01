7fdf96777824143f657e97ff0eeaf48b16ac48cb
unknown <shahidulislam7150@gmail.com>
2022-08-01 17:14:11 +0600

    about page add img

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,9 +3,13 @@ import React from 'react';
 const About = () => {
     return (
         <div className='w-50 mx-auto'>
-            <h3>About C2FSTORE </h3>
-            <p>C2F is the grocery product suppler.The organization grew up with a brand of organic vegetables.We always want to satisfy our daily customers .We never supply formalin including product .Our product is 100% safe for human body. </p>
+            <h3 className='text-center my-3'>About F2C-STORE </h3>
+            <img width="100%" className='rounded-pill about my-3' src="https://www.eatthis.com/wp-content/uploads/sites/4/2020/07/aisle.jpg?quality=82&strip=1" alt="" />
+            
+           <div className='bg-white shadow p-3 m-3'>
+           <p>C2F is the grocery product suppler.The organization grew up with a brand of organic vegetables.We always want to satisfy our daily customers .We never supply formalin including product .Our product is 100% safe for human body. </p>
             <p>We start our business 2012.Now we are one of the famous organic food supplier in the Bangladesh.</p>
+           </div>
         </div>
     );
 };
