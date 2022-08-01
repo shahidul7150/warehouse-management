477a1df504442a81e451be2b916b4826309859aa
unknown <shahidulislam7150@gmail.com>
2022-08-01 13:36:51 +0600

    add anounce color

--- src/components/Home/Announce.js
+++ src/components/Home/Announce.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Announce.css';
 const Announce = () => {
-    const [announceStyle, setAnnounceStyle] = useState('bg-success text-white d-flex justify-items-center justify-content-between px-5 display-6')
+    const [announceStyle, setAnnounceStyle] = useState('background1 text-white d-flex justify-items-center justify-content-between px-5 display-6')
     
     const handleClose = () => {
         setAnnounceStyle(announceStyle + " d-none")
@@ -9,7 +9,7 @@ const Announce = () => {
     return (
         <div className={announceStyle}>
             <h1>Hurry up it's 40% off now</h1>
-            <h2 className='cursor-pointer' onClick={handleClose}>X</h2>
+            <h2 className='cursor-pointer' onClick={handleClose}>x</h2>
            
         </div>
     );
--- src/components/Home/OptimizeInventory/OptimizeInv.js
+++ src/components/Home/OptimizeInventory/OptimizeInv.js
@@ -9,19 +9,19 @@ const OptimizeInv = () => {
                     <img src="https://www.earningdiary.com/wp-content/uploads/2019/07/Manage-Stock-Using-the-Right-Inventory-Management-Tools.png" alt="" />
                 </div>
                 <div className='optimize-article'>
-                    <div >
+                    <div className='bg-white p-3 shadows'>
                         <h4>Flexible control</h4>
                         <p>Use the inventory method that makes sense fot your types of warehouse business</p>
                     </div>
-                    <div>
+                    <div className='bg-white p-3 shadows'>
                         <h4>Full Visibility</h4>
                         <p>Keep just the amount to meet demand, eliminate stock-outs,never lose sight of inventory</p>
                     </div>
-                    <div>
+                    <div className='bg-white p-3 shadows'>
                         <h4>Accurate Costing</h4>
                         <p>Know your profitability and track actual costs,not average inventory value</p>
                     </div>
-                    <div>
+                    <div className='bg-white p-3 shadows'>
                         <h4>Inventory Planning</h4>
                         <p>Tap a wealth of data to get the right mix of products and channels</p>
                     </div>
