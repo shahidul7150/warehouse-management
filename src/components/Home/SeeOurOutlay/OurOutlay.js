e69319e2b7cdf0a238b82c1a85a6832847c93fd0
unknown <shahidulislam7150@gmail.com>
2022-08-01 16:54:36 +0600

    map added

--- src/components/Home/SeeOurOutlay/OurOutlay.js
+++ src/components/Home/SeeOurOutlay/OurOutlay.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import './Outlay.css';
 const OurOutlay = () => {
   
     return (
-                <div className='mt-5 bg-secondary p-5'>
+                <div className='mt-5  p-5'>
             
 
                     <h3 className='text-center text-white mb-4 '>Our Outlay</h3>
-                <div className="google-map-code   container">
+                <div className="google-map-code   container background-map  ">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58894.10243465049!2d90.31878461995015!3d22.695458476913224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x37553407fbece487%3A0x5d069b9599d4414a!2sBarishal!5e0!3m2!1sen!2sbd!4v1651477769739!5m2!1sen!2sbd" width="100%" height="450" style={{ border: 0 }}  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
                 </div>
